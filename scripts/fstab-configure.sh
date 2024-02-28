b9e326edc1ce6f2f8c1dcb3139b17d45e475e924
TannerSchultz2006 <tks92955@bethel.edu>
2024-02-27 20:35:02 -0600

    extension to the previous commit with fstab config and slurm config

--- scripts/fstab-configure.sh
+++ scripts/fstab-configure.sh
@@ -5,9 +5,8 @@ sudo su
 echo -n "Welcome to Pi Node Setup <MUST BE RUN IN SUDO>"
 
 HOSTNAME=$(hostname)
-MASTER_IP_ADDR=$(cat /etc/hosts | grep master | awk '{print $1}')
-IP_SCHEMA=$(cat /etc/hosts | grep master | awk '{print $1}')
-IP_SCHEMA=${IP_SCHEMA:0:8}
+MASTER_IP_ADDR=$(awk 'master {print $1}' /etc/hosts)
+IP_SCHEMA=${MASTER_IP_ADDR%.*$}
 
 #-------- apt installs ----------
 
@@ -15,7 +14,7 @@ apt install ntpdate -y
 
 #------- nfs mounting -----------
 
-if [ "$HOSTNAME" == "master" ]; then
+if [ "$(awk -v "$HOSTNAME" '$0~hostname {print $1}')" == "$MASTER_IP_ADDR" ]; then
     apt install nfs-kernel-server -y
     UUID=$(Blkid -s UUID -o value -t LABEL="/clusterfs")
 
@@ -23,16 +22,15 @@ if [ "$HOSTNAME" == "master" ]; then
     chown nobody.nogroup /clusterfs
     chmod -R 777 /clusterfs
 
-    echo "UUID=$UUID /clusterfs ext4 defaults 0 2" | tee -a /etc/fstab
+    echo "UUID=${UUID} /clusterfs ext4 defaults 0 2" | tee -a /etc/fstab
     mount -a
 
     chown nobody.nogroup -R /clusterfs
     chmod -R 766 /clusterfs
 
-    echo "/clusterfs    $IP_SCHEMA.0/24 (rw,sync,no_root_squash,no_subtree_check)" | tee -a /etc/exports
+    echo "/clusterfs    ${IP_SCHEMA}0/24 (rw,sync,no_root_squash,no_subtree_check)" | tee -a /etc/exports
 
     exportfs -a
-
 
 else
     apt install nfs-common -y
@@ -41,7 +39,7 @@ else
     chown nobody.nogroup /clusterfs
     chmod -R 777 /clusterfs
 
-    echo "$MASTER_IP_ADDR:/clusterfs    /clusterfs    nfs    defaults    0 0" | tee -a /etc/fstab
+    echo "${MASTER_IP_ADDR}:/clusterfs    /clusterfs    nfs    defaults    0 0" | tee -a /etc/fstab
     mount -a
 
 fi
