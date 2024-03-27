b17ebf4c18396caf34c97b0112669b6b585fb095
TannerSchultz2006 <tks92955@bethel.edu>
2024-03-27 10:19:08 -0500

    Modifications to fstab

--- scripts/configure-fstab.sh
+++ scripts/configure-fstab.sh
@@ -5,8 +5,11 @@ source ./utils.sh
 MASTER_IP_ADDR=$(getconfval cluster_master)
 IP_SCHEMA=$(getconfval ip_submask)
 DEBUG=$(getconfval DEBUG)
+NODE_COUNT=$(getconfval number_of_nodes)
 IS_DB=$(getconfval is_db)
 
+NODE_LIST=('')
+BASE_NAME=$(getconfval cluster_name)
 
 if [[ "$DEBUG" != "" ]]; then
     DEBUG="true"
@@ -29,7 +32,14 @@ if [[ "$IS_DB" == "true" ]]; then
     awk '{if ($0 !~ /clusterfs/) {print $0}}' /etc/exports > exports.tmp
     echo "/clusterfs    "$IP_SCHEMA"(rw,sync,no_root_squash,no_subtree_check)" >> exports.tmp
 
+    for ((i = 0; i < $NODE_COUNT; i++)); do
+        NODE_LIST+=($BASE_NAME$i)
+    done
+    cp /etc/hosts.allow hosts.allow.tmp
+    echo "nfs: ${NODE_LIST[@]}" >> hosts.allow.tmp
+
     if [ "$DEBUG" == "false" ]; then
+        sudo mv hosts.allow.tmp /etc/hosts.allow
         sudo apt install nfs-kernel-server -y
         sudo mkdir /clusterfs
         sudo chown nobody.nogroup /clusterfs
--- tests/scripts/configure-fstab.sh
+++ tests/scripts/configure-fstab.sh
@@ -5,8 +5,11 @@ source ./utils.sh
 MASTER_IP_ADDR=$(getconfval cluster_master)
 IP_SCHEMA=$(getconfval ip_submask)
 DEBUG=$(getconfval DEBUG)
+NODE_COUNT=$(getconfval number_of_nodes)
 IS_DB=$(getconfval is_db)
 
+NODE_LIST=('')
+BASE_NAME=$(getconfval cluster_name)
 
 if [[ "$DEBUG" != "" ]]; then
     DEBUG="true"
@@ -27,9 +30,16 @@ if [[ "$IS_DB" == "true" ]]; then
     echo "UUID=${UUID} /clusterfs ext4 defaults 0 2" >> fstab.tmp
 
     awk '{if ($0 !~ /clusterfs/) {print $0}}' /etc/exports > exports.tmp
-    echo "/clusterfs    $IP_SCHEMA (rw,sync,no_root_squash,no_subtree_check)" >> exports.tmp
+    echo "/clusterfs    "$IP_SCHEMA"(rw,sync,no_root_squash,no_subtree_check)" >> exports.tmp
+
+    for ((i = 0; i < $NODE_COUNT; i++)); do
+        NODE_LIST+=($BASE_NAME$i)
+    done
+    cp /etc/hosts.allow hosts.allow.tmp
+    echo "nfs: ${NODE_LIST[@]}" >> hosts.allow.tmp
 
     if [ "$DEBUG" == "false" ]; then
+        sudo mv hosts.allow.tmp /etc/hosts.allow
         sudo apt install nfs-kernel-server -y
         sudo mkdir /clusterfs
         sudo chown nobody.nogroup /clusterfs
